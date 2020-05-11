63cf3f14ba4d359a839e53ed0e9e4ebc9578a94b
Maximillian Oertel <9854279+nebulabyss@users.noreply.github.com>
2020-05-11 20:58:48 +0200

    Implemented JS form generation

--- bird_count_add.php
+++ bird_count_add.php
@@ -12,36 +12,16 @@ require_once "./html/header.php";
     <?php require_once "./html/nav.php";?>
     <div>
         <h3 class="text-muted mt-2">Bird count</h3>
-        <form class="form">
+        <form>
             <div class="form-row mb-2">
                 <div class="col-2">
                     <input type="text" class="form-control bg-warning text-dark" placeholder="Batch Date" id="datepicker" name="date">
                 </div>
             </div>
             <fieldset disabled>
-            <div class="form-row mb-2">
-                <div class="col">
-                    <input id="c_name" type="text" class="form-control" placeholder="Common Name" name="name_0">
+                <div class="form-body">
+
                 </div>
-                <div class="col">
-                    <input type="text" class="form-control" placeholder="Count" name="count_0">
-                </div>
-                <div class="col">
-                    <input type="text" class="form-control" placeholder="Behaviour" name="behave_0">
-                </div>
-                <div class="col">
-                    <input type="text" class="form-control" placeholder="Habitat" name="hab_0">
-                </div>
-                <div class="col">
-                    <input type="text" class="form-control" placeholder="Latitude" name="lat_0">
-                </div>
-                <div class="col">
-                    <input type="text" class="form-control" placeholder="Longitude" name="long_0">
-                </div>
-                <div class="col">
-                    <input type="text" class="form-control" placeholder="Zone" name="zone_0" id="zone">
-                </div>
-            </div>
             </fieldset>
         </form>
         <div class="float-right">
@@ -52,42 +32,52 @@ require_once "./html/header.php";
     </div>
 
     <script>
+        var fieldCount = 0;
+        function generateForm(fc) {
+            formHtml = '<div class="form-row mb-2"> \
+                          <div class="col"> \
+                            <input id="c_name" type="text" class="form-control" placeholder="Common Name" name="name_' + fc + '"> \
+                      </div> \
+                      <div class="col"> \
+                        <input type="text" class="form-control" placeholder="Count" name="count_' + fc + '"> \
+                      </div> \
+                      \<div class="col"> \
+                        <input type="text" class="form-control" placeholder="Behaviour" name="behave_' + fc + '"> \
+                      </div> \
+                      \<div class="col"> \
+                        <input type="text" class="form-control" placeholder="Habitat" name="hab_' + fc + '"> \
+                      </div> \
+                      \<div class="col"> \
+                        <input type="text" class="form-control" placeholder="Latitude" name="lat_' + fc + '"> \
+                      </div> \
+                      \<div class="col"> \
+                        <input type="text" class="form-control" placeholder="Longitude" name="long_' + fc + '"> \
+                      </div> \
+                      <div class="col"> \
+                        <input type="text" class="form-control" placeholder="Zone" name="zone_' + fc + '" id="zone"> \
+                      </div> \
+            </div>'
+            return formHtml;
+        }
         $(document).ready(function () {
-            $( document ).on( "keydown", "#c_name", function() {
+
+
+            $( '.form-body' ).append(
+                generateForm(fieldCount)
+            );
+
+            $( document ).on( 'keydown', '#c_name', function() {
                 $(this).autocomplete({
-                    source: "bird_c_name.php"
+                    source: 'bird_c_name.php'
                 });
             });
-            fieldCount = 0;
-            $( document ).on( "keydown", "#zone", function(e) {
-                var keyCode = e.keyCode || e.which;
+            $( document ).on( 'keydown', '#zone', function( event ) {
+                var keyCode = event.keyCode || event.which;
                 if (keyCode === 9) {
                     fieldCount++;
 
-                    $( ".form" ).append(
-                        '<div class="form-row mb-2"> \
-                                  <div class="col"> \
-                                    <input id="c_name" type="text" class="form-control" placeholder="Common Name" name="name_' + fieldCount + '"> \
-                      </div> \
-                      <div class="col"> \
-                        <input type="text" class="form-control" placeholder="Count" name="count_' + fieldCount + '"> \
-                      </div> \
-                      \<div class="col"> \
-                        <input type="text" class="form-control" placeholder="Behaviour" name="behave_' + fieldCount + '"> \
-                      </div> \
-                      \<div class="col"> \
-                        <input type="text" class="form-control" placeholder="Habitat" name="hab_' + fieldCount + '"> \
-                      </div> \
-                      \<div class="col"> \
-                        <input type="text" class="form-control" placeholder="Latitude" name="lat_' + fieldCount + '"> \
-                      </div> \
-                      \<div class="col"> \
-                        <input type="text" class="form-control" placeholder="Longitude" name="long_' + fieldCount + '"> \
-                      </div> \
-                      <div class="col"> \
-                        <input type="text" class="form-control" placeholder="Zone" name="zone_' + fieldCount + '" id="zone"> \
-                      </div> \
-            </div>'
+                    $( '.form-body' ).append(
+                        generateForm(fieldCount)
                     );
                     $(this).focus();
                 };
@@ -97,13 +87,12 @@ require_once "./html/header.php";
         $( '#datepicker' ).datepicker({
             dateFormat:  "yy-mm-dd"
         });
-        $('#datepicker').change( function () {
+        $( '#datepicker' ).change( function () {
             $('fieldset').prop('disabled', false);
             $('input[name="name_0"]').focus();
         });
 
     </script>
-    </div>
 </div>
 
 <?php require_once "./html/footer.php";?>
