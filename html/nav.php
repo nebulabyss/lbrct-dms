bdc66f131c6ee7db5573c58ee5bd9822c34d65b4
Maximillian Oertel <9854279+nebulabyss@users.noreply.github.com>
2020-05-11 08:25:41 +0200

    Initial commit

---
+++ html/nav.php
@@ -0,0 +1,49 @@
+<nav class="navbar navbar-expand-md navbar-light bg-light">
+    <a class="navbar-brand" href="#">LBRCT <small class="text-muted d-sm-none d-md-inline d-none">Data Management System</small></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse mr-auto mt-2 mt-lg-0" id="navbarSupportedContent">
+        <span class="navbar-text ml-auto mr-2">Logged is as <span class="navbar-text text-dark" style="font-weight: bold"><?php echo $user?></span></span>
+        <button type="button" class="btn btn-secondary">Log out</button>
+    </div>
+</nav>
+
+<nav>
+    <ul class="nav bg-light p-2 border-bottom">
+        <li class="nav-item dropdown mr-2">
+            <a class="nav-link btn btn-outline-primary dropdown-toggle" data-toggle="dropdown" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Compliance
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#">Add boat inspection</a>
+                <a class="dropdown-item" href="#">Add slipway inspection</a>
+                <a class="dropdown-item" href="#">Add area usage count</a>
+            </div>
+        </li>
+
+        <li class="nav-item dropdown mr-2">
+            <a class="nav-link btn btn-outline-success dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Conservation
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#">Add bird count</a>
+                <a class="dropdown-item" href="#">Add water quality</a>
+                <a class="dropdown-item" href="#">Add marine debris</a>
+            </div>
+        </li>
+
+        <li class="nav-item dropdown">
+            <a class="nav-link btn btn-outline-info dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Reports
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#">Bird counts</a>
+                <a class="dropdown-item" href="#">Water quality</a>
+                <a class="dropdown-item" href="#">Marine debris</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">Boat inspections</a>
+                <a class="dropdown-item" href="#">Slipway inspections</a>
+                <a class="dropdown-item" href="#">Area usage count</a>
+        </li>
+    </ul>
