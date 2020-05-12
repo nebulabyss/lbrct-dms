374fbafbac867b368f294f0706d9206e27ab70b0
Maximillian Oertel <9854279+nebulabyss@users.noreply.github.com>
2020-05-12 08:58:28 +0200

    Changed field names to array format

--- bird_count_add.php
+++ bird_count_add.php
@@ -23,44 +23,77 @@ require_once "./html/header.php";
 
                 </div>
             </fieldset>
-        </form>
+
+        <button type="button" class="btn btn-info" data-toggle="modal" data-target="#addSpeciesModal">Add species</button>
         <div class="float-right">
             <button type="button" class="btn btn-danger">Cancel</button>
             <button type="button" class="btn btn-success">Submit</button>
         </div>
-
+        </form>
     </div>
+    <!-- Modal -->
+    <div class="modal fade" id="addSpeciesModal" tabindex="-1" role="dialog" aria-labelledby="addSpeciesModalTitle" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title text-muted" id="addSpeciesModalTitle">Add bird species</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form id="addSpecies" method="post">
+                        <div class="form-row">
+                            <div class="col">
+                                <input type="text" class="form-control" name="species_name" placeholder="Common Name">
+                            </div>
+                            <div class="form-group col">
+                                <select id="inputState" name="species_type" class="form-control">
+                                    <option selected>Water-associated</option>
+                                    <option>Terrestrial</option>
+                                </select>
+                            </div>
+                        </div>
 
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                    <button type="submit" class="btn btn-success">Save changes</button>
+                </div>
+                </form>
+            </div>
+        </div>
+    </div>
     <script>
         var fieldCount = 0;
         function generateForm(fc) {
             formHtml = '<div class="form-row mb-2"> \
                           <div class="col"> \
-                            <input id="c_name" type="text" class="form-control" placeholder="Common Name" name="name_' + fc + '"> \
+                            <input id="c_name" type="text" class="form-control" placeholder="Common Name" name="row[' + fc + '][c_name]"> \
                       </div> \
                       <div class="col"> \
-                        <input type="text" class="form-control" placeholder="Count" name="count_' + fc + '"> \
+                        <input type="text" class="form-control" placeholder="Count" name="row[' + fc + '][count]"> \
                       </div> \
                       \<div class="col"> \
-                        <input type="text" class="form-control" placeholder="Behaviour" name="behave_' + fc + '"> \
+                        <input type="text" class="form-control" placeholder="Behaviour" name="row[' + fc + '][behavior]"> \
                       </div> \
                       \<div class="col"> \
-                        <input type="text" class="form-control" placeholder="Habitat" name="hab_' + fc + '"> \
+                        <input type="text" class="form-control" placeholder="Habitat" name="row[' + fc + '][habitat]"> \
                       </div> \
                       \<div class="col"> \
-                        <input type="text" class="form-control" placeholder="Latitude" name="lat_' + fc + '"> \
+                        <input type="text" class="form-control" placeholder="Latitude" name="row[' + fc + '][lat]"> \
                       </div> \
                       \<div class="col"> \
-                        <input type="text" class="form-control" placeholder="Longitude" name="long_' + fc + '"> \
+                        <input type="text" class="form-control" placeholder="Longitude" name="row[' + fc + '][long]"> \
                       </div> \
                       <div class="col"> \
-                        <input type="text" class="form-control" placeholder="Zone" name="zone_' + fc + '" id="zone"> \
+                        <input type="text" class="form-control" placeholder="Zone" name="row[' + fc + '][zone]" id="zone"> \
                       </div> \
             </div>'
             return formHtml;
         }
-        $(document).ready(function () {
 
+        $(document).ready(function () {
 
             $( '.form-body' ).append(
                 generateForm(fieldCount)
@@ -87,10 +120,39 @@ require_once "./html/header.php";
         $( '#datepicker' ).datepicker({
             dateFormat:  "yy-mm-dd"
         });
+
         $( '#datepicker' ).change( function () {
             $('fieldset').prop('disabled', false);
             $('input[name="name_0"]').focus();
         });
+
+        $( '#addSpecies' ).on( "submit", function( event ) {
+            event.preventDefault();
+            $.ajax({
+                type: "POST",
+                url: 'add_species.php',
+                data: $(this).serialize()
+            })
+                .done( function( response ) {
+                    {
+                        var jsonData = JSON.parse(response);
+
+                        // response
+                        if (jsonData.success == "1") {
+                            // TODO: success message
+                            $(function () {
+                                $('#addSpeciesModal').modal('toggle');
+                                $('input[name="species_name"').val('');
+                                $('#inputState').prop("selectedIndex", 0);
+                            });
+                        } else {
+                            // TODO: error message
+                        }
+                    }
+                });
+            });
+
+
 
     </script>
 </div>
