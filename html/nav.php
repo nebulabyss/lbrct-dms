06070ea4d6c17b21533b8df787021cdf5904f65d
Maximillian Oertel <9854279+nebulabyss@users.noreply.github.com>
2020-05-16 14:13:50 +0200

    Removed duplicate attribute

--- html/nav.php
+++ html/nav.php
@@ -15,7 +15,7 @@
 <nav class="">
     <ul class="nav bg-light p-2 border-bottom">
         <li class="nav-item dropdown mr-2">
-            <a class="nav-link btn btn-outline-primary dropdown-toggle" data-toggle="dropdown" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <a class="nav-link btn btn-outline-primary dropdown-toggle" data-toggle="dropdown" href="#" id="navbarDropdown" role="button" aria-haspopup="true" aria-expanded="false">
                 Compliance
             </a>
             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
