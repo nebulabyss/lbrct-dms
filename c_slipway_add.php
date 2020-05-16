4c777fd6452341df349bd726c049b58116881176
Maximillian Oertel <9854279+nebulabyss@users.noreply.github.com>
2020-05-16 22:11:13 +0200

    Updated batch field set

--- c_slipway_add.php
+++ c_slipway_add.php
@@ -21,25 +21,9 @@ $zones = $stmt->fetchAll(PDO::FETCH_KEY_PAIR );
         <h3 class="text-muted mt-2">Boat patrol inspection</h3>
         <form>
             <div class="form-row mb-2">
-                <div class="col-1">
-                    <input type="text" class="form-control bg-warning text-dark" placeholder="Batch Date" id="datepicker" name="date" required>
+                <div class="col-2">
+                    <input type="text" class="form-control bg-warning text-dark" placeholder="Batch Date" id="datepicker" name="date">
                 </div>
-
-                <fieldset class="form-row" disabled>
-
-                    <div class="form-group row">
-                        <label for="time" class="col ml-1 col-form-label">Start Time:</label>
-                    </div>
-                    <div class="col">
-                        <input type="time" class="form-control bg-warning text-dark" name="time_start" required>
-                    </div>
-                    <div class="form-group row">
-                        <label for="time" class="col col-form-label">End Time:</label>
-                    </div>
-                    <div class="col">
-                        <input type="time" class="form-control bg-warning text-dark" name="time_end" required>
-                    </div>
-                </fieldset>
             </div>
             <fieldset disabled>
                 <div class="form-body">
@@ -48,6 +32,35 @@ $zones = $stmt->fetchAll(PDO::FETCH_KEY_PAIR );
             </fieldset>
         </form>
     </div>
+    <!-- Modal -->
+    <div class="modal fade" id="addSpeciesModal" tabindex="-1" role="dialog" aria-labelledby="addSpeciesModalTitle" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title text-muted" id="addSpeciesModalTitle">Add bird species</h5>
+                </div>
+                <div class="modal-body">
+                    <form id="addSpecies" method="post">
+                        <div class="form-row">
+                            <div class="col">
+                                <input type="text" class="form-control" name="species_name" placeholder="Common Name">
+                            </div>
+                            <div class="form-group col">
+                                <select id="inputState" name="species_type" class="form-control">
+                                    <option selected>Water-associated</option>
+                                    <option>Terrestrial</option>
+                                </select>
+                            </div>
+                        </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                    <button type="submit" class="btn btn-success">Save changes</button>
+                </div>
+                </form>
+            </div>
+        </div>
+    </div>
     <script>
         let lineNum = 1;
         let rowCount = 0;
@@ -55,14 +68,7 @@ $zones = $stmt->fetchAll(PDO::FETCH_KEY_PAIR );
             formHtml = '<div class="form-row mb-2"> \
                        <label class="col-form-label d-inline-block text-center" style="width: 30px;">' + ln + '</label> \
                           <div class="col"> \
-                        <select class="form-control" name="row[' + rc + '][zone]" id="zone"><option selected value="">Zone</option> \ <?php
-                foreach($zones as $k => $v):
-                    echo ('<option value="' . $k . '">' . $v . '</option>');
-                endforeach;
-                ?> </select> \
-                    </div> \
-                      <div> \
-                      <input id="c_name" type="text" class="form-control" placeholder="Breede Reg." name="row[' + rc + '][breede]"> \
+                            <input id="c_name" type="text" class="form-control" placeholder="Breede Reg." name="row[' + rc + '][breede]"> \
                       </div> \
                       <div class="col"> \
                         <input type="text" class="form-control" placeholder="Licence No." name="row[' + rc + '][licence]"> \
@@ -84,7 +90,15 @@ $zones = $stmt->fetchAll(PDO::FETCH_KEY_PAIR );
                         <input type="text" class="form-control" placeholder="Warning" name="row[' + rc + '][warning]"> \
                       </div> \
                       <div class="col"> \
-                        <input type="text" id="final" class="form-control" placeholder="Fine" name="row[' + rc + '][fine]"> \
+                        <input type="text" class="form-control" placeholder="Fine" name="row[' + rc + '][fine]"> \
+                      </div> \
+                      <div class="col"> \
+                        <select class="form-control" name="row[' + rc + '][zone]" id="zone"><option selected value="">Zone</option> \ <?php
+                foreach($zones as $k => $v):
+                    echo ('<option value="' . $k . '">' . $v . '</option>');
+                endforeach;
+                ?>
+                </select> \
                       </div> \
             </div>'
             return formHtml;
@@ -101,7 +115,7 @@ $zones = $stmt->fetchAll(PDO::FETCH_KEY_PAIR );
                     source: 'bird_c_name.php'
                 });
             });
-            $( document ).on( 'keydown', '#final', function( event ) {
+            $( document ).on( 'keydown', '#zone', function( event ) {
                 var keyCode = event.keyCode || event.which;
                 if (keyCode === 9) {
                     rowCount++;
