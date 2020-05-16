dc9e3f320d09143d56e02a332a0c17645e742fc8
Maximillian Oertel <9854279+nebulabyss@users.noreply.github.com>
2020-05-16 22:11:53 +0200

    Slipways initial implementation

--- c_slipway_add.php
+++ c_slipway_add.php
@@ -7,7 +7,7 @@ session_start();
 require_once "./html/header.php";
 
 
-$stmt = $pdo->prepare('SELECT compliance_zones_id, description FROM compliance_zones');
+$stmt = $pdo->prepare('SELECT slipways_id, description FROM slipways');
 $stmt->execute(array());
 $zones = $stmt->fetchAll(PDO::FETCH_KEY_PAIR );
 
@@ -18,12 +18,35 @@ $zones = $stmt->fetchAll(PDO::FETCH_KEY_PAIR );
 <div class="container-fluid">
     <?php require_once "./html/nav.php";?>
     <div>
-        <h3 class="text-muted mt-2">Boat patrol inspection</h3>
+        <h3 class="text-muted mt-2">Slipway inspection</h3>
         <form>
             <div class="form-row mb-2">
-                <div class="col-2">
-                    <input type="text" class="form-control bg-warning text-dark" placeholder="Batch Date" id="datepicker" name="date">
+                <div class="col-1">
+                    <input type="text" class="form-control bg-warning text-dark" placeholder="Batch Date" id="datepicker" name="date" required>
                 </div>
+
+                <fieldset class="form-row" disabled>
+                    <div class="col-auto">
+                        <select class="form-control bg-warning" name="row[' + rc + '][zone]" id="zone"><option selected value="">Slipway</option>
+                            <?php
+                            foreach($zones as $k => $v):
+                                echo ('<option value="' . $k . '">' . $v . '</option>');
+                            endforeach;
+                            ?> </select>
+                    </div>
+                    <div class="form-group row">
+                        <label for="time" class="col col-form-label">Start Time:</label>
+                    </div>
+                    <div class="col">
+                        <input type="time" class="form-control bg-warning text-dark" name="time_start" required>
+                    </div>
+                    <div class="form-group row">
+                        <label for="time" class="col col-form-label">End Time:</label>
+                    </div>
+                    <div class="col">
+                        <input type="time" class="form-control bg-warning text-dark" name="time_end" required>
+                    </div>
+                </fieldset>
             </div>
             <fieldset disabled>
                 <div class="form-body">
@@ -32,49 +55,35 @@ $zones = $stmt->fetchAll(PDO::FETCH_KEY_PAIR );
             </fieldset>
         </form>
     </div>
-    <!-- Modal -->
-    <div class="modal fade" id="addSpeciesModal" tabindex="-1" role="dialog" aria-labelledby="addSpeciesModalTitle" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title text-muted" id="addSpeciesModalTitle">Add bird species</h5>
-                </div>
-                <div class="modal-body">
-                    <form id="addSpecies" method="post">
-                        <div class="form-row">
-                            <div class="col">
-                                <input type="text" class="form-control" name="species_name" placeholder="Common Name">
-                            </div>
-                            <div class="form-group col">
-                                <select id="inputState" name="species_type" class="form-control">
-                                    <option selected>Water-associated</option>
-                                    <option>Terrestrial</option>
-                                </select>
-                            </div>
-                        </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-                    <button type="submit" class="btn btn-success">Save changes</button>
-                </div>
-                </form>
-            </div>
-        </div>
-    </div>
     <script>
         let lineNum = 1;
         let rowCount = 0;
         function generateForm(rc, ln) {
             formHtml = '<div class="form-row mb-2"> \
                        <label class="col-form-label d-inline-block text-center" style="width: 30px;">' + ln + '</label> \
-                          <div class="col"> \
-                            <input id="c_name" type="text" class="form-control" placeholder="Breede Reg." name="row[' + rc + '][breede]"> \
+                       <div class=""> \
+                        <label for="time" class="col col-form-label font-weight-bold">Time:</label> \
+                        </div> \
+                        <div class="col-auto"> \
+                        <input type="time" class="form-control text-dark" name="time" required> \
+                        </div> \
+                      <div class="col"> \
+                      <input id="c_name" type="text" class="form-control" placeholder="Activity" name="row[' + rc + '][activity]"> \
+                      </div> \
+                      <div class="col"> \
+                        <input type="text" class="form-control" placeholder="Vehicle Reg." name="row[' + rc + '][vreg]"> \
+                      </div> \
+                      <div class="col"> \
+                        <input type="text" class="form-control" placeholder="SAMSA" name="row[' + rc + '][samsa]"> \
+                      </div> \
+                      <div class="col"> \
+                        <input type="text" class="form-control" placeholder="Boat Name" name="row[' + rc + '][bname]"> \
                       </div> \
                       <div class="col"> \
                         <input type="text" class="form-control" placeholder="Licence No." name="row[' + rc + '][licence]"> \
                       </div> \
                       <div class="col"> \
-                        <input type="text" class="form-control" placeholder="SAMSA" name="row[' + rc + '][samsa]"> \
+                        <input type="text" id="final" class="form-control" placeholder="Fine" name="row[' + rc + '][fine]"> \
                       </div> \
                       <div class="col"> \
                         <input type="text" class="form-control" placeholder="Engine size" name="row[' + rc + '][size]"> \
@@ -82,23 +91,6 @@ $zones = $stmt->fetchAll(PDO::FETCH_KEY_PAIR );
                       <div class="form-check big-checkbox my-auto ml-2 mr-1"> \
                       <input class="form-check-input" type="checkbox" id="gridCheck" name="row[' + rc + '][twin]"> \
                       <label class="form-check-label font-weight-bold ml-1" for="gridCheck">Twin</label> \
-                      </div> \
-                      <div class="col"> \
-                        <input type="text" class="form-control" placeholder="Offence" name="row[' + rc + '][offence]"> \
-                      </div> \
-                      <div class="col"> \
-                        <input type="text" class="form-control" placeholder="Warning" name="row[' + rc + '][warning]"> \
-                      </div> \
-                      <div class="col"> \
-                        <input type="text" class="form-control" placeholder="Fine" name="row[' + rc + '][fine]"> \
-                      </div> \
-                      <div class="col"> \
-                        <select class="form-control" name="row[' + rc + '][zone]" id="zone"><option selected value="">Zone</option> \ <?php
-                foreach($zones as $k => $v):
-                    echo ('<option value="' . $k . '">' . $v . '</option>');
-                endforeach;
-                ?>
-                </select> \
                       </div> \
             </div>'
             return formHtml;
@@ -115,7 +107,7 @@ $zones = $stmt->fetchAll(PDO::FETCH_KEY_PAIR );
                     source: 'bird_c_name.php'
                 });
             });
-            $( document ).on( 'keydown', '#zone', function( event ) {
+            $( document ).on( 'keydown', '#final', function( event ) {
                 var keyCode = event.keyCode || event.which;
                 if (keyCode === 9) {
                     rowCount++;
