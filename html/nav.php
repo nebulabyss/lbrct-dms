f76b725052d6b5b5aab287577a42b947d00c07f5
Maximillian Oertel <9854279+nebulabyss@users.noreply.github.com>
2020-09-19 22:54:32 +0200

    Updated hyperlinks

--- html/nav.php
+++ html/nav.php
@@ -1,12 +1,12 @@
 <nav class="navbar navbar-expand-md navbar-light bg-light border-bottom shadow-sm">
-    <a class="navbar-brand" href="index.php">LBRCT <small class="text-muted d-sm-none d-md-inline d-none">Data Management System</small></a>
+    <a class="navbar-brand" href="../index.php">LBRCT <small class="text-muted d-sm-none d-md-inline d-none">Data Management System</small></a>
     <?php if (isset($_SESSION['user_id'])): ?>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse mr-auto mt-2 mt-lg-0" id="navbarSupportedContent">
         <span class="navbar-text ml-auto mr-2">Signed in as <span class="text-dark" style="font-weight: bold"><?php echo $_SESSION[user_name];?></span></span>
-        <a type="button" href='logout.php' class="btn btn-secondary ml-2 text-white">Sign out</a>
+        <a type="button" href='../logout.php' class="btn btn-secondary ml-2 text-white">Sign out</a>
     </div>
     <?php endif; ?>
 </nav>
@@ -19,9 +19,9 @@
                 Compliance
             </a>
             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="boatpatrols_add.php">Add boat inspection</a>
-                <a class="dropdown-item" href="c_slipway_add.php">Add slipway inspection</a>
-                <a class="dropdown-item" href="c_zone_count_add.php">Add zone usage count</a>
+                <a class="dropdown-item" href="../boatpatrols_add.php">Add boat inspection</a>
+                <a class="dropdown-item" href="../c_slipway_add.php">Add slipway inspection</a>
+                <a class="dropdown-item" href="../c_zone_count_add.php">Add zone usage count</a>
             </div>
         </li>
 
@@ -30,8 +30,8 @@
                 Conservation
             </a>
             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="bird_count_add.php">Add bird count</a>
-                <a class="dropdown-item" href="marine_debris_add.php">Add marine debris</a>
+                <a class="dropdown-item" href="../bird_count_add.php">Add bird count</a>
+                <a class="dropdown-item" href="../marine_debris_add.php">Add marine debris</a>
                 <a class="dropdown-item" href="#">Add water quality</a>
             </div>
         </li>
@@ -52,4 +52,4 @@
         </li>
     </ul>
 </nav>
-    <?php endif; ?>
+<?php endif; ?>
