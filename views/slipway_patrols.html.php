ade663a1acd7268729d8565cf8d46d65e15acdcf
Maximillian Oertel <9854279+nebulabyss@users.noreply.github.com>
2020-09-21 22:46:34 +0200

    Modified keydown jQuery

--- views/slipway_patrols.html.php
+++ views/slipway_patrols.html.php
@@ -99,7 +99,6 @@
             );
 
             $( document ).on( 'keydown', '#final', function( event ) {
-                $(this).attr('id', '');
                 const keyCode = event.keyCode || event.which;
                 if (keyCode === 9) {
                     rowCount++;
@@ -108,6 +107,7 @@
                     $( '.form-body' ).append(
                         generateForm(rowCount, lineNum)
                     );
+                    $(this).attr('id', '');
                     $(this).focus();
                 }
             });
