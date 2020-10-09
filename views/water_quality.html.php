3a614e2bbcd07028d8fe346b5df6d1e8bc9197de
Maximillian Oertel <9854279+nebulabyss@users.noreply.github.com>
2020-10-09 13:33:22 +0200

    Updated checkbox classes

--- views/water_quality.html.php
+++ views/water_quality.html.php
@@ -1,45 +1,57 @@
 <?php if (isset($wq_data)):?>
     <div class="container-fluid">
     <?php include "includes/nav.php";?>
-    <table class="table table-bordered mt-2">
-        <thead>
-        <tr>
-            <th scope="col">Mark</th>
-            <th scope="col">Date / Time</th>
-            <th scope="col">Time</th>
-            <th scope="col">RDO Concentration (mg/L)</th>
-            <th scope="col">RDO Saturation (%Sat)</th>
-            <th scope="col">Temperature (°C)</th>
-            <th scope="col">Specific Conductivity (mS/cm)</th>
-            <th scope="col">Salinity (PSU)</th>
-            <th scope="col">Depth (m)</th>
-            <th scope="col">pH (pH)</th>
-        </tr>
-        </thead>
-        <tbody>
-        <?php
-        $i = 0;
-        foreach ($wq_data as $k):
-            if (!empty($marked)) {
-                if (in_array($i, $marked)) {
+    <form action="../water_quality.php" method="post">
+        <table class="table table-bordered mt-2">
+            <thead>
+            <tr>
+                <th scope="col">Mark</th>
+                <th scope="col">Date</th>
+                <th scope="col">Time</th>
+                <th scope="col">RDO Concentration (mg/L)</th>
+                <th scope="col">RDO Saturation (%Sat)</th>
+                <th scope="col">Temperature (°C)</th>
+                <th scope="col">Specific Conductivity (mS/cm)</th>
+                <th scope="col">Salinity (PSU)</th>
+                <th scope="col">Depth (m)</th>
+                <th scope="col">pH (pH)</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php
+            $rowCounter = 0;
+            foreach ($wq_data as $k):
+                if ($k['marked'] == 'Marked') {
                     echo '<tr class="text-white bg-info">';
                 } else {
                     echo '<tr>';
                 }
-            }
-            echo '<td>';
-            echo '<input class="form-check-input mx-auto" type="checkbox" id="gridCheck" name="row[' . $i . ']">';
-            echo '</td>';
-
-            foreach ($k as $v):
-                echo '<td>' . $v . '</td>';
+                echo '<td style="text-align: center;">';
+                echo '<input class="big-checkbox" type="checkbox" name="row[' . $rowCounter . ']">';
+                echo '</td>';
+                unset($k['marked']);
+                foreach ($k as $v):
+                    echo '<td>' . $v . '</td>';
+                endforeach;
+                echo '</tr>';
+                $rowCounter++;
             endforeach;
-            echo '</tr>';
-            $i++;
-        endforeach;
-        ?>
-        </tbody>
-    </table>
+            ?>
+            </tbody>
+        </table>
+        <div class="float-right mb-4">
+            <button type="button" class="btn btn-danger">Cancel</button>
+            <button type="submit" class="btn btn-success">Submit</button>
+        </div>
+    </form>
+        <script>
+            $('input[type=checkbox').on('click', function (){
+                if ($(this).closest('tr').hasClass('bg-info')) {
+                    $(this).closest('tr').removeClass('text-white bg-info');
+                }
+                    $(this).closest('tr').toggleClass('text-white bg-success');
+            });
+        </script>
     </div>
 <?php else: ?>
 <body>
@@ -49,24 +61,25 @@
         <h3 class="text-muted mt-2">Water quality</h3>
         <form action="../water_quality.php" method="post" enctype="multipart/form-data">
             <div class="form-row mb-2">
-                    <div class="col-1">
-                        <label for="datepicker" class="ui-helper-hidden"></label><input type="text" class="form-control bg-warning" placeholder="Batch Date" id="datepicker" name="date" required>
-                    </div>
-                    <div>
-                        <label for="start_time" class="col ml-1 col-form-label">Start Time:</label>
-                    </div>
-                    <div class="col-1">
-                        <input type="time" class="form-control bg-warning" id="start_time" name="start_time" required>
-                    </div>
-                    <div>
-                        <label for="end_time" class="col col-form-label">End Time:</label>
-                    </div>
-                    <div class="col-1">
-                        <input type="time" class="form-control bg-warning" id="end_time" name="end_time" required>
-                    </div>
+                <div class="col-1">
+                    <label for="datepicker" class="ui-helper-hidden"></label>
+                    <input type="text" class="form-control bg-warning" placeholder="Batch Date" id="datepicker" name="date"
+                    <?php if (isset($_SESSION['date'])) echo 'value="' . $_SESSION['date'] . '"' ?> required>
+                </div>
+                <div class="col-2">
+                    <label for="site" class="ui-helper-hidden"></label>
+                    <select class="form-control custom-select bg-warning" name="site" required><option selected value="">Site</option>
+                        <?php
+                        if (isset($sites)) {
+                            foreach($sites as $k => $v):
+                                echo ('<option value="' . $k . '">' . $k . ' - ' .$v . '</option>');
+                            endforeach;
+                        }
+                        ?> </select>
+                </div>
             </div>
             <div class="custom-file col-4">
-                <input type="file" name="userfile" class="custom-file-input" id="customFile">
+                <input type="file" name="userfile" class="custom-file-input" id="customFile" required>
                 <label class="custom-file-label" for="customFile">Choose file</label>
             </div>
             <div>
@@ -76,13 +89,13 @@
 
         <script>
             // Name of the file appears on select
-            $(".custom-file-input").on("change", function() {
+            $(".custom-file-input").on('change', function() {
                 var fileName = $(this).val().split("\\").pop();
                 $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
             });
             let date_picker = $( '#datepicker' );
             date_picker.datepicker({
-                dateFormat:  "yy-mm-dd"
+                dateFormat:  'yy-mm-dd'
             });
 
             date_picker.change( function () {
