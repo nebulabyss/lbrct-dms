d7c7e355a9c2569824c94aa3a0d76134fec2bf62
Maximillian Oertel <9854279+nebulabyss@users.noreply.github.com>
2020-10-11 14:05:08 +0200

    Initial implementation

---
+++ classes/VuSituXHTMLUpload.php
@@ -0,0 +1,54 @@
+<?php
+
+
+class VuSituXHTMLUpload
+{
+    private array $files_array;
+    private string $uploaddir;
+    private string $uploadfile;
+
+    function __construct($files_array)
+    {
+        $this->files_array = $files_array;
+        $this->uploaddir = __DIR__ . '/../uploads/';
+        $this->uploadfile = $this->uploaddir . basename($this->files_array['userfile']['name']);
+    }
+
+    public function MoveUploadedFile()
+    {
+        move_uploaded_file($this->files_array['userfile']['tmp_name'], $this->uploadfile);
+    }
+
+    public function ParseXHTML() {
+        $dom = new DomDocument();
+        $dom->loadHTMLFile($this->uploadfile, LIBXML_NOERROR);
+
+        $finder = new DomXPath($dom);
+        $classnames = ['data', 'data-marked'];
+        $trList = $finder->query("//tr[contains(concat(' ', normalize-space(@class), ' '), ' $classnames[0] ') or contains(concat(' ', normalize-space(@class), ' '), ' $classnames[1] ')]");
+
+        $keys = array('time', 'rdocon', 'rdosat', 'temp', 'cond', 'sal', 'depth', 'ph', 'marked');
+        $wq_data = array();
+        foreach ($trList as $tr) {
+            $row = ['site' => $_POST['site']];
+            $counter = 0;
+            foreach ($tr->getElementsByTagName("td") as $td) {
+                $string = trim($td->textContent);
+                if ($counter == 0) {
+                    $token = strpos($string, ' ');
+                    if (!isset($wq_data['date'])) {
+                        $wq_data['date'] = substr($string, 0, $token);
+                    }
+                    $row[$keys[$counter]] = substr($string, $token);
+                    $counter++;
+                    continue;
+                }
+                $row[$keys[$counter]] = $string;
+                $counter++;
+            }
+            $wq_data['row'][] = $row;
+        }
+        unlink($this->uploadfile);
+        return $wq_data;
+    }
+}
