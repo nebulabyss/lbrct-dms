4784153a7ff7cc009387fbb20b6d3907a7e6fa74
Maximillian Oertel <9854279+nebulabyss@users.noreply.github.com>
2020-10-11 12:03:49 +0200

    Implemented new WQMarkedElementCleanUp method

--- classes/FormProcessor.php
+++ classes/FormProcessor.php
@@ -3,10 +3,10 @@
 
 class FormProcessor
 {
-    protected array $post_data;
+    protected array $user_data;
 
-    function __construct($post_data) {
-        $this->post_data = $post_data;
+    function __construct($user_data) {
+        $this->user_data = $user_data;
     }
 
     public function FormElementCleanUp() {
@@ -14,38 +14,56 @@ class FormProcessor
          * Clean up elements that expect integers in database and set checkboxes to integer 1.
          */
         $counter = 0;
-        while ($counter < count($this->post_data['row'])) {
-            foreach ($this->post_data['row'][$counter] as $k => $v) {
-                if ($this->post_data['row'][$counter][$k] == '') {
-                    $this->post_data['row'][$counter][$k] = NULL;
+        while ($counter < count($this->user_data['row'])) {
+            foreach ($this->user_data['row'][$counter] as $k => $v) {
+                if ($this->user_data['row'][$counter][$k] == '') {
+                    $this->user_data['row'][$counter][$k] = NULL;
 
                     continue;
                 }
-                if ($this->post_data['row'][$counter][$k] == 'on') {
-                    $this->post_data['row'][$counter][$k] = 1;
+                if ($this->user_data['row'][$counter][$k] == 'on') {
+                    $this->user_data['row'][$counter][$k] = 1;
                 }
             }
             $counter++;
         }
     }
 
+    public function WQMarkedElementCleanUp($marked) {
+        $counter = 0;
+        while ($counter < count($this->user_data['row'])) {
+            $this->user_data['row'][$counter]['marked'] = NULL;
+            $counter++;
+        }
+
+        foreach ($marked as $key) {
+            $this->user_data['row'][$key]['marked'] = 1;
+        }
+    }
+
     public function ProcessForm($db_object, $batch_table, $db_table) {
         // Process batch
         $batch_data = array();
-        foreach ($this->post_data as $k => $v) {
+        foreach ($this->user_data as $k => $v) {
             if ($k == 'row') {
                 break;
             }
             $batch_data[$k] = $v;
         }
-        $db_object->InsertIntoDatabase($batch_data, $batch_table);
 
-        $last_batch_id = $db_object->GetLastInsertID();
+        $check_batch = $db_object->CheckIfBatchExists($batch_data['date'], $batch_table);
+
+        if ($check_batch) {
+            $last_batch_id = (int)$check_batch;
+        } else {
+            $db_object->InsertIntoDatabase($batch_data, $batch_table);
+            $last_batch_id = $db_object->GetLastInsertID();
+        }
 
         // Process form rows
         $counter = 0;
-        while ($counter < count($this->post_data['row'])) {
-            $db_object->InsertIntoDatabase($this->post_data['row'][$counter], $db_table, $last_batch_id);
+        while ($counter < count($this->user_data['row'])) {
+            $db_object->InsertIntoDatabase($this->user_data['row'][$counter], $db_table, $last_batch_id);
             $counter++;
         }
     }
