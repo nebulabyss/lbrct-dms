bac707508898a8a8e229aa73106fccd3c38526ea
Maximillian Oertel <9854279+nebulabyss@users.noreply.github.com>
2020-10-11 14:06:20 +0200

    Implemented VuSituXHTMLUpload class

--- water_quality.php
+++ water_quality.php
@@ -1,5 +1,6 @@
 <?php
 include 'pdo.php';
+include 'classes/VuSituXHTMLUpload.php';
 include 'classes/FormProcessor.php';
 include 'classes/DatabaseController.php';
 session_start();
@@ -10,49 +11,10 @@ session_start();
 $database_controller = new DatabaseController($pdo);
 
 if (isset($_FILES['userfile'])) {
-    $uploaddir = __DIR__ . '/uploads/';
-    $uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
-
-    move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile);
-
-    $dom = new DomDocument();
-    $dom->loadHTMLFile($uploadfile, LIBXML_NOERROR);
-
-    $wq_data = array();
-    $keys = array('time', 'rdocon', 'rdosat', 'temp', 'cond', 'sal', 'depth', 'ph', 'marked');
-    $finder = new DomXPath($dom);
-    $classnames = ['data', 'data-marked'];
-
-    $trList = $dom->getElementsByTagName('tr');
-    $trList = $finder->query("//tr[contains(concat(' ', normalize-space(@class), ' '), ' $classnames[0] ') or contains(concat(' ', normalize-space(@class), ' '), ' $classnames[1] ')]");
-
-    foreach ($trList as $tr) {
-        $row = ['site' => $_POST['site']];
-        $counter = 0;
-        foreach ($tr->getElementsByTagName("td") as $td) {
-            $string = trim($td->textContent);
-            if ($counter == 0) {
-                $token = strpos($string, ' ');
-                if (!isset($wq_data['date'])) {
-                    $wq_data['date'] = substr($string, 0, $token);
-                }
-                $row[$keys[$counter]] = substr($string, $token);
-                $counter++;
-                continue;
-            }
-
-            if ($string == '') $string = NULL;
-            if ($string == 'Marked') $string = 1;
-
-            $row[$keys[$counter]] = $string;
-            $counter++;
-        }
-        $wq_data['row'][] = $row;
-
-    }
-
+    $upload = new VuSituXHTMLUpload($_FILES);
+    $upload->MoveUploadedFile();
+    $wq_data = $upload->ParseXHTML();
     $_SESSION['wq_data'] = $wq_data;
-    unlink($uploadfile);
 
 } elseif (isset($_POST['row'])) {
     $marked = array_keys($_POST['row']);
@@ -61,7 +23,6 @@ if (isset($_FILES['userfile'])) {
 
     $batch_table = 'water_quality_batch';
     $db_table = 'water_quality';
-
     $form_processor->ProcessForm($database_controller, $batch_table, $db_table);
 
     unset($_SESSION['wq_data']);
