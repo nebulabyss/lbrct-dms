5cbcc13ad1fd78180fff67e3de650ad1178cdb0d
Maximillian Oertel <9854279+nebulabyss@users.noreply.github.com>
2020-10-12 09:43:39 +0200

    Removed else from if statement

--- boat_patrol.php
+++ boat_patrol.php
@@ -18,20 +18,19 @@ if (isset($_POST['date'])) {
     $db_table = 'boat_patrol';
     $form_processor->ProcessForm($database_controller, $batch_table, $db_table);
 
-} else {
-    /*
-     * Use an array of arrays.
-     * The first element per array is the database table.
-     * Subsequent elements are the relevant columns.
-     */
-    $table_columns = array(
-        array('compliance_zones', 'compliance_zones_id', 'description'),
-        array('transgression_types', 'transgression_id', 'section')
-    );
-    $zones = $database_controller->SelectKeyPairs($table_columns[0]);
-    $trans = $database_controller->SelectKeyPairs($table_columns[1]);
-
-    include 'includes/header.php';
-    include 'views/boat_patrol.html.php';
-    include 'includes/footer.php';
 }
+/*
+ * Use an array of arrays.
+ * The first element per array is the database table.
+ * Subsequent elements are the relevant columns.
+ */
+$table_columns = array(
+    array('compliance_zones', 'compliance_zones_id', 'description'),
+    array('transgression_types', 'transgression_id', 'section')
+);
+$zones = $database_controller->SelectKeyPairs($table_columns[0]);
+$trans = $database_controller->SelectKeyPairs($table_columns[1]);
+
+include 'includes/header.php';
+include 'views/boat_patrol.html.php';
+include 'includes/footer.php';
