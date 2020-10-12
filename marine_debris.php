cf08027b257633b8a849e22368a606a8b8d7c20d
Maximillian Oertel <9854279+nebulabyss@users.noreply.github.com>
2020-10-12 03:53:41 +0200

    Split controller and view

---
+++ marine_debris.php
@@ -0,0 +1,30 @@
+<?php
+include 'pdo.php';
+include 'classes/FormProcessor.php';
+include 'classes/DatabaseController.php';
+session_start();
+
+/**
+ * @var $pdo
+ */
+$database_controller = new DatabaseController($pdo);
+if (isset($_POST['date'])) {
+    $form_processor = new FormProcessor($_POST);
+    $form_processor->FormElementCleanUp();
+    /*
+     * Specify relevant batch table and form table as strings.
+     */
+    $batch_table = 'marine_debris_batch';
+    $db_table = 'marine_debris';
+    $form_processor->ProcessForm($database_controller, $batch_table, $db_table);
+}
+
+$table_columns = array(
+    array('marine_debris_zones', 'marine_debris_zones_id', 'description')
+);
+$item_codes = $database_controller->MarineDebrisCodes();
+$zones = $database_controller->SelectKeyPairs($table_columns[0]);
+
+include 'includes/header.php';
+include 'views/marine_debris.html.php';
+include 'includes/footer.php';
--- views/marine_debris.html.php
+++ views/marine_debris.html.php
@@ -1,38 +1,28 @@
-<?php
-require 'pdo.php';
-session_start();
-include "./includes/header.php";
-
-$stmt = $pdo->prepare('SELECT marine_debris_minor_categories.marine_debris_minor_categories_id, marine_debris_minor_categories.code, marine_debris_minor_categories.description, marine_debris_major_categories.description FROM marine_debris_minor_categories INNER JOIN marine_debris_major_categories WHERE marine_debris_minor_categories.marine_debris_major_categories_id = marine_debris_major_categories.marine_debris_major_categories_id;
-');
-$stmt->execute(array());
-$item_codes = $stmt->fetchAll(PDO::FETCH_NAMED);
-
-$stmt = $pdo->prepare('SELECT marine_debris_zones_id, description FROM marine_debris_zones');
-$stmt->execute(array());
-$zones = $stmt->fetchAll(PDO::FETCH_KEY_PAIR );
-?>
-
 <body>
 <div class="container-fluid">
     <?php include "./includes/nav.php";?>
     <div>
         <h3 class="text-muted mt-2">Marine debris</h3>
-        <form>
+        <form action="../marine_debris.php" method="post">
             <div class="form-row mb-2">
                 <div class="col-1">
+                    <label for="datepicker" class="ui-helper-hidden"></label>
                     <input type="text" class="form-control bg-warning text-dark" placeholder="Batch Date" id="datepicker" name="date" required>
                 </div>
 
                 <fieldset class="form-row" disabled>
                 <div class="col">
-                    <select class="form-control bg-warning text-dark custom-select" name="zone" required><option selected value="">Zone</option>
-                        <?php
-                        foreach($zones as $k => $v):
-                            echo ('<option value="' . $k . '">' . $v . '</option>');
-                        endforeach;
-                        ?>
-                    </select>
+                    <label>
+                        <select class="form-control bg-warning text-dark custom-select" name="zone" id="zone" required><option selected value="">Zone</option>
+                            <?php
+                            if (isset($zones)) {
+                                foreach($zones as $k => $v):
+                                    echo ('<option value="' . $k . '">' . $v . '</option>');
+                                endforeach;
+                            }
+                            ?>
+                        </select>
+                    </label>
                 </div>
                 </fieldset>
             </div>
@@ -41,7 +31,6 @@ $zones = $stmt->fetchAll(PDO::FETCH_KEY_PAIR );
 
                 </div>
             </fieldset>
-
         <div class="float-right">
             <button type="button" class="btn btn-danger">Cancel</button>
             <button type="submit" class="btn btn-success">Submit</button>
@@ -56,9 +45,11 @@ $zones = $stmt->fetchAll(PDO::FETCH_KEY_PAIR );
                        <label class="col-form-label d-inline-block text-center" style="width: 30px;">' + ln + '</label> \
                                                 <div class="col"> \
                         <select class="form-control custom-select" name="row[' + rc + '][item]" required><option selected value="">Item</option> \ <?php
-                foreach($item_codes as $a):
-                    echo ('<option value="' . $a['marine_debris_minor_categories_id'] . '">' . $a['code'] . ' - ' . $a['description'][0].  ' - ' . $a['description'][1].'</option>');
-                endforeach;
+                if (isset($item_codes)) {
+                    foreach($item_codes as $a):
+                        echo ('<option value="' . $a['marine_debris_minor_categories_id'] . '">' . $a['code'] . ' - ' . $a['description'][0].  ' - ' . $a['description'][1].'</option>');
+                    endforeach;
+                }
                 ?>
                         </select> \
                       </div> \
@@ -69,7 +60,7 @@ $zones = $stmt->fetchAll(PDO::FETCH_KEY_PAIR );
                         <input type="text" class="form-control" placeholder="Latitude" name="row[' + rc + '][lat]"> \
                       </div> \
                       <div class="col"> \
-                        <input type="text" id="long" class="form-control" placeholder="Longitude" name="row[' + rc + '][long]"> \
+                        <input type="text" id="longt" class="form-control" placeholder="Longitude" name="row[' + rc + '][longt]"> \
                       </div> \
             </div>'
             return formHtml;
@@ -81,8 +72,8 @@ $zones = $stmt->fetchAll(PDO::FETCH_KEY_PAIR );
                 generateForm(rowCount, lineNum)
             );
 
-            $(document).on('keydown', '#long', function (event) {
-                var keyCode = event.keyCode || event.which;
+            $(document).on('keydown', '#longt', function (event) {
+                let keyCode = event.keyCode || event.which;
                 if (keyCode === 9) {
                     rowCount++;
                     lineNum++;
@@ -95,16 +86,14 @@ $zones = $stmt->fetchAll(PDO::FETCH_KEY_PAIR );
             });
         });
 
-        $( '#datepicker' ).datepicker({
+        let date_picker = $( '#datepicker' );
+        date_picker.datepicker({
             dateFormat:  "yy-mm-dd"
         });
 
-        $( '#datepicker' ).change( function () {
+        date_picker.change( function () {
             $('fieldset').prop('disabled', false);
-            $('select[name="zone"]').focus();
+            $('#zone').focus();
         });
     </script>
 </div>
-
-<?php include "./includes/footer.php";?>
-
