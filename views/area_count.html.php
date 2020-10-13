e981e2b660f1bea558e49e0747f048047fef65ef
Maximillian Oertel <9854279+nebulabyss@users.noreply.github.com>
2020-10-13 14:25:23 +0200

    Modified date picker column width

--- views/area_count.html.php
+++ views/area_count.html.php
@@ -4,7 +4,7 @@
         <h3 class="text-muted mt-2">Area count</h3>
         <form action="../area_count.php" method="post">
             <div class="form-row mb-2">
-                <div class="col-auto">
+                <div class="col-1">
                     <label for="datepicker" class="ui-helper-hidden"></label>
                     <input type="text" class="form-control bg-warning text-dark" placeholder="Batch Date" id="datepicker" name="date" required>
                 </div>
