5e10a9bfa49ee5d23565f71e17b644f5366f3327
Maximillian Oertel <9854279+nebulabyss@users.noreply.github.com>
2020-10-16 10:07:24 +0200

    Refactored ParseXHTML method

--- classes/VuSituXHTML.php
+++ classes/VuSituXHTML.php
@@ -28,27 +28,27 @@ class VuSituXHTML
         $keys = array('time', 'rdocon', 'rdosat', 'temp', 'cond', 'sal', 'depth', 'ph', 'marked');
         $wq_data = array();
         $wq_data['site'] = $_POST['site'];
+
+        $date = $trList->item(0)->childNodes->item(0)->textContent;
+        $needle = strpos($date, ' ');
+        $wq_data['date'] = substr($date, 0, $needle);
+
         $tr_length = false;
+        if ($trList->item(0)->childNodes->length > 9) {
+            $tr_length = true;
+        }
+
         foreach ($trList as $tr) {
-            if ($tr->childNodes->length > 9) {
-                $tr_length = true;
+            if ($tr_length) {
+                $tr->removeChild($tr->childNodes->item(8));
+                $tr->removeChild($tr->childNodes->item(8));
             }
+
             $row = [];
             $counter = 0;
             foreach ($tr->getElementsByTagName("td") as $td) {
-                if ($tr_length && $counter == 8 || $counter == 9) {
-                    $counter++;
-                    continue;
-                }
-                if ($counter == 10) {
-                    $counter = 8;
-                }
                 $string = trim($td->textContent);
                 if ($counter == 0) {
-                    $needle = strpos($string, ' ');
-                    if (!isset($wq_data['date'])) {
-                        $wq_data['date'] = substr($string, 0, $needle);
-                    }
                     $row[$keys[$counter]] = substr($string, $needle);
                     $counter++;
                     continue;
