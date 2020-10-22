fa41889b32aae15dbd4faf61020b722a68987526
Maximillian Oertel <9854279+nebulabyss@users.noreply.github.com>
2020-10-22 19:49:00 +0200

    Initial commit

---
+++ js/date_report.js.php
@@ -0,0 +1,9 @@
+let date_picker = $('.datepicker');
+date_picker.datepicker({
+    dateFormat:  "yy-mm-dd"
+});
+
+date_picker.change(function() {
+    $('fieldset').prop('disabled', false);
+    $('#next').focus();
+});
