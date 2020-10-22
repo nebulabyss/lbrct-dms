6b226dbe1052076277e35abdb37b7076b5c163d8
Maximillian Oertel <9854279+nebulabyss@users.noreply.github.com>
2020-10-22 22:50:21 +0200

    Implemented date validation

--- js/date_report.js.php
+++
@@ -1,9 +0,0 @@
-let date_picker = $('.datepicker');
-date_picker.datepicker({
-    dateFormat:  "yy-mm-dd"
-});
-
-date_picker.change(function() {
-    $('fieldset').prop('disabled', false);
-    $('#next').focus();
-});
---
+++ js/report.js.php
@@ -0,0 +1,22 @@
+let date_picker = $('.datepicker');
+date_picker.datepicker({
+    dateFormat:  "yy-mm-dd"
+});
+
+$('#submit').on('click', function (e) {
+    var startId = $('#start_date');
+    var endId = $('#end_date');
+    var startDate = startId.val();
+    var endDate = endId.val();
+    if (startDate > endDate) {
+        e.preventDefault();
+        $('.alert').remove();
+        let message = `<div class="alert alert-danger alert-dismissible fade show" role="alert">
+            <strong>ERROR:</strong> <u>From</u> date is greater than <u>To</u> date.
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>`;
+        $('h3').before(message);
+    }
+});
