7014d5af215bc1bc0485918ea35e81a922e13c65
Maximillian Oertel <9854279+nebulabyss@users.noreply.github.com>
2020-10-23 18:47:33 +0200

    New method call

--- zone_count_report.php
+++ zone_count_report.php
@@ -13,8 +13,10 @@ if (isset($_POST['start_date'])) {
         array('compliance_zones', 'description'));
 
     $zones = $database_controller->SelectColumn($table_columns[0]);
-    $zone_count = $database_controller->zoneCountReport($_POST['start_date'], $_POST['end_date']);
+    $zone_count = $database_controller->zoneCountReportSum($_POST['start_date'], $_POST['end_date']);
+    $zone_max_per_day = $database_controller->zoneCountReportMax($_POST['start_date'], $_POST['end_date']);
 }
+
 include 'includes/header.php';
 include 'views/zone_count_report.html.php';
 include 'includes/footer.php';
