3ae54d7cd616bf0f2fa057b5be439ae2be646583
Maximillian Oertel <9854279+nebulabyss@users.noreply.github.com>
2020-10-23 23:18:34 +0200

    Added SVG icons

--- includes/report_buttons.php
+++ includes/report_buttons.php
@@ -1,2 +1,19 @@
-<button type="button" class="btn btn-success float-right" id="submit">Export as CSV</button>
-<button type="button" class="btn btn-info float-right mr-1" id="copy" data-toggle="tooltip" data-placement="bottom" title="Copied!" data-clipboard-target="#clipboard" data-clipboard-action="copy">Copy to clipboard</button>
+<button type="button" class="btn btn-success float-right" id="submit">Export as CSV
+    <svg style="vertical-align: top;" width="1.3em" height="1.3em" viewBox="0 0 16 16" class="bi bi-file-earmark-spreadsheet" fill="currentColor"
+         xmlns="http://www.w3.org/2000/svg">
+        <path fill-rule="evenodd" d="M5 10H3V9h10v1h-3v2h3v1h-3v2H9v-2H6v2H5v-2H3v-1h2v-2zm1 0v2h3v-2H6z"/>
+        <path d="M4 0h5.5v1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4.5h1V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2z"/>
+        <path d="M9.5 3V0L14 4.5h-3A1.5 1.5 0 0 1 9.5 3z"/>
+    </svg>
+</button>
+<button type="button" class="btn btn-info float-right mr-1" id="copy" data-toggle="tooltip" data-placement="bottom"
+        title="Copied!" data-clipboard-target="#clipboard" data-clipboard-action="copy">Copy to clipboard
+    <svg style="vertical-align: top;" width="1.3em" height="1.3em" viewBox="0 0 16 16" class="bi bi-clipboard-check"
+         fill="currentColor"
+         xmlns="http://www.w3.org/2000/svg">
+        <path fill-rule="evenodd"
+              d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/>
+        <path fill-rule="evenodd"
+              d="M9.5 1h-3a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3zm4.354 7.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708 0z"/>
+    </svg>
+</button>
