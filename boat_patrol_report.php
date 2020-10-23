4b930998764cf2c0d6b266b5e1ab0d62e3c096b1
Maximillian Oertel <9854279+nebulabyss@users.noreply.github.com>
2020-10-23 15:00:12 +0200

    Initial commit

---
+++ boat_patrol_report.php
@@ -0,0 +1,21 @@
+<?php
+include 'pdo.php';
+include 'classes/DatabaseController.php';
+session_start();
+
+/**
+ * @var $pdo
+ */
+$database_controller = new DatabaseController($pdo);
+
+if (isset($_POST['start_date'])) {
+    $table_columns = array(
+        array('compliance_zones', 'description'));
+
+    $patrols = $database_controller->boatPatrolReport($_POST['start_date'], $_POST['end_date']);
+}
+
+include 'includes/header.php';
+include 'views/boat_patrol_report.html.php';
+include 'includes/footer.php';
+
