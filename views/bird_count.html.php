27e00965765799d7774e021cd6577197ed1e4daa
Maximillian Oertel <9854279+nebulabyss@users.noreply.github.com>
2020-10-26 12:26:38 +0200

    Cleaned up stray HTML tags

--- views/bird_count.html.php
+++ views/bird_count.html.php
@@ -6,48 +6,51 @@
         <form action="../bird_count.php" method="post">
             <div class="form-row mb-2">
                 <?php include './includes/date_picker.php' ?>
+                <fieldset class="form-row" disabled>
+                    <div class="col">
+                        <label for="next" class="ui-helper-hidden"></label>
+                        <select class="form-control bg-warning custom-select" id="next" name="zone" required>
+                            <option selected value="">Zone</option>
+                            <?php
+                            if (isset($zones)) {
+                                foreach ($zones as $k => $v):
+                                    echo('<option value="' . $k . '">' . $v . '</option>');
+                                endforeach;
+                            }
+                            ?>
+                        </select>
+                    </div>
+                    <div>
+                        <label for="start_time" class="col-form-label">Start Time:</label>
+                    </div>
+                    <div class="col">
+                        <input type="time" class="form-control bg-warning" id="start_time" name="start_time" required>
+                    </div>
+                    <div>
+                        <label for="end_time" class="col-form-label">End Time:</label>
+                    </div>
+                    <div class="col">
+                        <input type="time" class="form-control bg-warning" id="end_time" name="end_time" required>
+                    </div>
+                </fieldset>
             </div>
-            <fieldset class="form-row" disabled>
-                <div class="col">
-                    <label for="next" class="ui-helper-hidden"></label>
-                    <select class="form-control bg-warning custom-select" id="next" name="zone" required>
-                        <option selected value="">Zone</option>
-                        <?php
-                        if (isset($zones)) {
-                            foreach ($zones as $k => $v):
-                                echo('<option value="' . $k . '">' . $v . '</option>');
-                            endforeach;
-                        }
-                        ?>
-                    </select>
-                </div>
-                <div>
-                    <label for="start_time" class="col-form-label">Start Time:</label>
-                </div>
-                <div class="col">
-                    <input type="time" class="form-control bg-warning" id="start_time" name="start_time" required>
-                </div>
-                <div>
-                    <label for="end_time" class="col-form-label">End Time:</label>
-                </div>
-                <div class="col">
-                    <input type="time" class="form-control bg-warning" id="end_time" name="end_time" required>
+
+
+            <fieldset disabled>
+                <div class="form-body" id="form-body">
+
                 </div>
             </fieldset>
-    </div>
-    <fieldset disabled>
-        <div class="form-body" id="form-body">
-
-        </div>
-    </fieldset>
-    <button type="button" class="btn btn-info" data-toggle="modal" data-target="#addSpeciesModal">Add species</button>
-    <?php include './includes/remove_row_botton.php' ?>
-    <div class="float-right">
-        <?php include './includes/cancel_button.php' ?>
-        <button type="submit" class="btn btn-success">Submit</button>
+            <button type="button" class="btn btn-info" data-toggle="modal" data-target="#addSpeciesModal">Add species
+            </button>
+            <?php include './includes/remove_row_botton.php' ?>
+            <div class="float-right">
+                <?php include './includes/cancel_button.php' ?>
+                <button type="submit" class="btn btn-success">Submit</button>
+            </div>
+        </form>
     </div>
 </div>
-<!-- Modal Start -->
 <div class="modal fade" id="addSpeciesModal" tabindex="-1" role="dialog" aria-labelledby="addSpeciesModalTitle"
      aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered" role="document">
