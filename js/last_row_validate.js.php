4b9f3ac0f62aa22d0b18a0b57b67e9900e5e7702
Maximillian Oertel <9854279+nebulabyss@users.noreply.github.com>
2020-10-27 10:21:07 +0200

    Code cleanup

--- js/last_row_validate.js.php
+++ js/last_row_validate.js.php
@@ -1,33 +1,35 @@
 $('#submit').on('click', function (event) {
-    let lastRow = document.getElementById('row' + rowCount);
-    let fields = lastRow.getElementsByClassName('validate');
-    let fieldCount = 0;
-    for (let i = 0; i < fields.length; i++) {
-        if (fields[i].value === '') {
-            fieldCount++;
-        }
-        if (fields[i].type === 'checkbox' && fields[i].checked === false) {
-            fieldCount++;
-        }
-    }
+    clearEmptyRows(event)
+});
 
-    if (fieldCount < fields.length) {
-    } else if (rowCount === 0) {
-        event.preventDefault();
-        $('.alert').remove();
-        let message = `<div class="alert alert-danger alert-dismissible fade show" role="alert">
+function clearEmptyRows (event) {
+    for (let i = rowCount; i > 0; i--) {
+        let lastRow = document.getElementById('row' + i);
+        let fields = lastRow.getElementsByClassName('validate');
+        let fieldCount = 0;
+        for (let n = 0; n < fields.length; n++) {
+            if (fields[n].value === '') {
+                fieldCount++;
+            }
+            if (fields[n].type === 'checkbox' && fields[n].checked === false) {
+                fieldCount++;
+            }
+        }
+
+        if (fieldCount < fields.length) {
+        } else if (i === 0) {
+            event.preventDefault();
+            $('.alert').remove();
+            let message = `<div class="alert alert-danger alert-dismissible fade show" role="alert">
             <strong>ERROR:</strong> Form fields are empty.
             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
             </button>
         </div>`;
-        $('h3').before(message);
-    } else {
-        let lastFormDiv = '#row' + rowCount;
-        $(lastFormDiv).remove();
-        rowCount--;
-        lineNum--;
-        let mod = '#' + triggerID + (rowCount);
-        $(mod).attr('id', triggerID);
+            $('h3').before(message);
+        } else {
+            let lastFormRowDiv = '#row' + i;
+            $(lastFormRowDiv).remove();
+        }
     }
-})
+}
