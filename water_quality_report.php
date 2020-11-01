27eb672f0cf90b7f8b7f564c5c5fdef35820155f
Maximillian Oertel <9854279+nebulabyss@users.noreply.github.com>
2020-11-01 13:00:08 +0200

    Added no_dates conditional variable

--- water_quality_report.php
+++ water_quality_report.php
@@ -9,12 +9,14 @@ include 'classes/DatabaseController.php';
 $database_controller = new DatabaseController($pdo);
 $title = 'Water quality report';
 $form_action = basename(__FILE__);
+$no_dates = false;
 
 if (isset($_POST['start_date'])) {
     $table_columns = array(
         array('compliance_zones', 'description'));
 
     $water_quality = $database_controller->WaterQuality($_POST['start_date'], $_POST['end_date']);
+    $no_dates = $water_quality;
 }
 
 include 'includes/header.php';
