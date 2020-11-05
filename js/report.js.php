1f3cba977b8199fc80aac2b036bbe85f8ed9cbbd
Maximillian Oertel <9854279+nebulabyss@users.noreply.github.com>
2020-11-05 12:01:51 +0200

    Added date range select

--- js/report.js.php
+++ js/report.js.php
@@ -1,36 +1,36 @@
 let start_date_picker = $('.start_datepicker');
 start_date_picker.datepicker({
-    dateFormat:  "yy-mm-dd",
+    dateFormat: "yy-mm-dd",
     showOtherMonths: true,
     selectOtherMonths: true,
     firstDay: 1,
     changeMonth: true,
     changeYear: true,
-    onChangeMonthYear: function(year, month){
-        $(this).datepicker( "setDate", year + '-' + month + '-01' );
+    onChangeMonthYear: function (year, month) {
+        $(this).datepicker("setDate", year + '-' + month + '-01');
     }
 });
 
-let date_picker = $('.datepicker');
-date_picker.datepicker({
-    dateFormat:  "yy-mm-dd",
+let end_date_picker = $('.datepicker');
+end_date_picker.datepicker({
+    dateFormat: "yy-mm-dd",
     showOtherMonths: true,
     selectOtherMonths: true,
     firstDay: 1,
     changeMonth: true,
     changeYear: true,
-    onChangeMonthYear: function(year, month){
-        var date = new Date(parseInt(year), parseInt(month) , 0).getDate();
-        $(this).datepicker( "setDate", year + '-' + month + '-' + date );
+    onChangeMonthYear: function (year, month) {
+        var date = new Date(parseInt(year), parseInt(month), 0).getDate();
+        $(this).datepicker("setDate", year + '-' + month + '-' + date);
     }
 });
 
-$('#start').click(function (){
-   $('#start_date').datepicker('show');
+$('#start').click(function () {
+    $('#start_date').datepicker('show');
 });
 
-$('#end').click(function (){
-   $('#end_date').datepicker('show');
+$('#end').click(function () {
+    $('#end_date').datepicker('show');
 });
 
 $('#submit').on('click', function (e) {
@@ -61,3 +61,81 @@ $('#copy').on('click', function () {
 });
 
 new ClipboardJS('#copy');
+
+let currentDate = new Date();
+let currentYear = currentDate.getFullYear();
+let currentMonth = currentDate.getMonth() + 1;
+let previousYear = currentYear - 1;
+let firstQuarter = ["-07-01", "-09-30"];
+let secondQuarter = ["-10-01", "-12-31"];
+let thirdQuarter = ["-01-01", "-03-31"];
+let fourthQuarter = ["-04-01", "-06-30"];
+let currentFinYearStart = 0;
+let currentFinYearEnd = 0;
+let previousFinYearStart = 0;
+let previousFinYearEnd = 0;
+
+dateRanges = function () {
+    if (currentMonth >= 7) {
+        currentFinYearStart = currentYear;
+        currentFinYearEnd = currentYear + 1;
+        previousFinYearStart = currentYear - 1;
+        previousFinYearEnd = currentYear;
+    }
+
+    if (currentMonth <= 6) {
+        currentFinYearStart = currentYear - 1;
+        currentFinYearEnd = currentYear;
+        previousFinYearStart = currentYear - 2;
+        previousFinYearEnd = currentYear - 1;
+    }
+
+    let rangeSelector = /** @lang HTML */
+        `
+            <option selected value="">Select date range</option>
+            <option value="0">Q1: July to September ${currentFinYearStart}</option>
+            <option value="1">Q2: October to December ${currentFinYearStart}</option>
+            <option value="2">Q3: January to March ${currentFinYearEnd}</option>
+            <option value="3">Q4: April to July ${currentFinYearEnd}</option>
+            <option value="4">Current Financial Year: ${currentFinYearStart} - ${currentFinYearEnd}</option>
+            <option value="5">Previous Financial Year: ${previousFinYearStart} - ${previousFinYearEnd}</option>
+
+        `;
+
+    $('#range-options').append(rangeSelector);
+}
+dateRanges();
+
+rangeOptions = $('#range-options');
+$(rangeOptions).on('change', function () {
+    if (rangeOptions.val() === "0") {
+        start_date_picker.val(currentFinYearStart + firstQuarter[0]);
+        end_date_picker.val(currentFinYearStart + firstQuarter[1]);
+    }
+
+    if (rangeOptions.val() === "1") {
+        start_date_picker.val(currentFinYearStart + secondQuarter[0]);
+        end_date_picker.val(currentFinYearStart + secondQuarter[1]);
+    }
+
+    if (rangeOptions.val() === "2") {
+        start_date_picker.val(currentFinYearEnd + thirdQuarter[0]);
+        end_date_picker.val(currentFinYearEnd + thirdQuarter[1]);
+    }
+
+    if (rangeOptions.val() === "3") {
+        start_date_picker.val(currentFinYearEnd + fourthQuarter[0]);
+        end_date_picker.val(currentFinYearEnd + fourthQuarter[1]);
+    }
+
+    if (rangeOptions.val() === "4") {
+        start_date_picker.val(currentFinYearStart + firstQuarter[0]);
+        end_date_picker.val(currentFinYearEnd + fourthQuarter[1]);
+    }
+
+    if (rangeOptions.val() === "5") {
+        start_date_picker.val(previousFinYearStart + firstQuarter[0]);
+        end_date_picker.val(previousFinYearEnd + fourthQuarter[1]);
+    }
+});
+
