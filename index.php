5f99adda1aa1e2f0793f465a1eb7093cd56f19c0
Maximillian Oertel <9854279+nebulabyss@users.noreply.github.com>
2020-11-05 07:49:37 +0200

    Added unprocessed transgression message

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
 include './includes/auth.php';
+include 'pdo.php';
+include 'classes/FormProcessor.php';
+include 'classes/DatabaseController.php';
 
 if (isset($_SESSION['TEMP'])) {
     unset($_SESSION['TEMP']);
@@ -9,10 +12,18 @@ if (!isset($_SESSION['USER_ID'])) {
     header('Location: login.php');
     exit;
 }
+
 include './includes/header.php';
+$database_controller = new DatabaseController($pdo);
+$boat_trans = $database_controller->CheckForTransgressions();
 ?>
-<body>
+    <body>
 <div class="container-fluid">
-<?php include './includes/nav.php';?>
+    <?php include './includes/nav.php'; ?>
+    <?php if ($boat_trans): ?>
+        <div>
+            <h4 class="text-danger mt-5 text-center">There are transgressions to be processed</h4>
+        </div>
+    <?php endif; ?>
 </div>
-<?php include './includes/footer.php';?>
+<?php include './includes/footer.php'; ?>
