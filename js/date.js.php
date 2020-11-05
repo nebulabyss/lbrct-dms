413901c0ac081ce5b1569b4f2a7b619512699724
Maximillian Oertel <9854279+nebulabyss@users.noreply.github.com>
2020-11-05 09:30:09 +0200

    Datepicker displays current and last month

--- js/date.js.php
+++ js/date.js.php
@@ -1,21 +1,20 @@
 let date_picker = $('#datepicker');
 date_picker.datepicker({
-    dateFormat:  "yy-mm-dd",
+    dateFormat: "yy-mm-dd",
     showOtherMonths: true,
     selectOtherMonths: true,
     firstDay: 1,
     changeMonth: true,
     changeYear: true,
-    onChangeMonthYear: function(year, month){
-        $(this).datepicker( "setDate", year + '-' + month + '-01' );
-    }
+    numberOfMonths: 2,
+    maxDate: "+0M"
 });
 
-date_picker.change(function() {
+date_picker.change(function () {
     $('fieldset').prop('disabled', false);
     $('#next').focus();
 });
 
-$('#cal-btn').click(function (){
+$('#cal-btn').click(function () {
     date_picker.datepicker('show');
 });
